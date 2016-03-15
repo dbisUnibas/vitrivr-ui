e10456bb694aeb869377272a4dd2a952ed87276b
Luca Rossetto <luca.rossetto@unibas.ch>
2016-03-15 11:08:05 +0100

    added some default values to the config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-var cineastHost = "";
-var thumbnailHost = "";
-var videoHost = "h";
+var cineastHost = "http://localhost/api.php";
+var thumbnailHost = "http://localhost/thumbnails";
+var videoHost = "http://localhost/videos";
 var thumbnailFileType = "jpg";
 var maxFrameGap = 250;
 var categories = ["globalcolor", "localcolor", "edge", "motion"];
