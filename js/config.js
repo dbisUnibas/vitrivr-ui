e032c46bbdbc8c50cbf6f83036e4c0816dafdbc7
Prateek <you@example.com>
2016-06-01 00:57:51 +0530

    changed var to const in config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 // config setting is according to my system please change accordingly
 
-var cineastHost = "http://localhost/vitrivr/vitrivr-ui/api.php";
-var thumbnailHost = "http://localhost/vitrivr/thumbnails/";
-var videoHost = "http://localhost/vitrivr/osvc_1.0/";
-var thumbnailFileType = "jpg";
-var maxFrameGap = 250;
-var categories = ["globalcolor", "localcolor", "edge", "motion"];
+const cineastHost = "http://localhost/vitrivr/vitrivr-ui/api.php";
+const thumbnailHost = "http://localhost/vitrivr/thumbnails/";
+const videoHost = "http://localhost/vitrivr/osvc_1.0/";
+const thumbnailFileType = "jpg";
+const maxFrameGap = 250;
