5a7354255ef2e311f1b4541006896977fcdb3de8
Luca Rossetto <luca.rossetto@unibas.ch>
2016-09-12 16:16:08 +0200

    fixed naming inconsistency in config

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@ const videoHost = "collection/";
 const thumbnailFileType = "jpg";
 const maxFrameGap = 250;
 const categoryConfig = {
-	globalColor:{displayName: "Global Color", defaultValue: 0.1},
-	localColor:{displayName: "Local Color", defaultValue: 0.6},
+	globalcolor:{displayName: "Global Color", defaultValue: 0.1},
+	localcolor:{displayName: "Local Color", defaultValue: 0.6},
 	edge:{displayName: "Edge", defaultValue: 0.3},
 	motion:{displayName: "Motion", defaultValue: 0}
 };
